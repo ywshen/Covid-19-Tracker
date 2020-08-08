151a7f98baae40fe90b9d95d45efa4c0be7cd486
ywshen <ywshen5002@gmail.com>
2020-08-07 21:19:57 -0400

    Adding map

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Map from './Map'
 import './App.css';
 import Table from './Table';
 import { sortData } from './util';
+import "leaflet/dist/leaflet.css"
 
 function App() {
     const [countries, setCountries] = useState([]);
     const [country, setCountry] = useState("worldwide");
     const [countryInfo, setCountryInfo] = useState({});
     const [tableData, setTableData] = useState([]);
+    const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+    const [mapZoom, setMapZoom] = useState(3);
 
     useEffect(() => {
         fetch("https://disease.sh/v3/covid-19/all")
@@ -87,7 +90,7 @@ function App() {
 
           </div>
 
-          <Map />
+          <Map center={mapCenter} zoom={mapZoom}/>
 
           </div>
           <Card className="app_right">
