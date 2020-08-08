56733d1dbd4f23e96d7cac14f780d56740425239
ywshen <ywshen5002@gmail.com>
2020-08-07 21:59:01 -0400

    Adding circles to map

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     const [tableData, setTableData] = useState([]);
     const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
     const [mapZoom, setMapZoom] = useState(3);
+    const [mapCountries, setMapCountries] = useState([]);
 
     useEffect(() => {
         fetch("https://disease.sh/v3/covid-19/all")
@@ -36,6 +37,7 @@ function App() {
 
                     const sortedData = sortData(data);
                     setTableData(sortedData);
+                    setMapCountries(data);
                     setCountries(countries);
                 });
         };
@@ -90,7 +92,12 @@ function App() {
 
           </div>
 
-          <Map center={mapCenter} zoom={mapZoom}/>
+          <Map 
+          countries={mapCountries}
+          casesType= "cases"
+          center={mapCenter}
+          zoom={mapZoom}
+          />
 
           </div>
           <Card className="app_right">
